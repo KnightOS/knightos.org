de82d8820dd6d64a2841910fe24fb188d82d6981
Drew DeVault <sir@cmpwn.com>
2014-05-21 11:37:48 -0600

    Fix jenkins download link generation on /download

--- js/download.js
+++ js/download.js
@@ -3,13 +3,14 @@ xhr.open('GET', 'http://builds.knightos.org/job/KnightOS/api/json');
 xhr.onload = function() {
   var buildStatus = JSON.parse(this.responseText);
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://builds.knightos.org/job/KnightOS/' + buildStatus.lastBuild.number + '/api/json');
+  xhr.open('GET', 'http://builds.knightos.org/job/KnightOS/' + buildStatus.lastSuccessfulBuild.number + '/api/json');
   xhr.onload = function() {
     var build = JSON.parse(this.responseText);
     var name = build.artifacts[0].fileName;
     var version = name.substr(9, name.length - 13);
     document.getElementById('current-version').textContent = "Version " + version;
-    document.getElementById('download-link').href = "http://builds.knightos.org/job/KnightOS/7/artifact/" + name;
+    document.getElementById('download-link').href = "http://builds.knightos.org/job/KnightOS/" +
+      buildStatus.lastSuccessfulBuild.number + "/artifact/" + name;
     document.getElementById('download-link').removeAttribute('disabled');
   };
   xhr.send();
