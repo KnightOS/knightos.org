7a4995b0f018bc59749f353c624f94780840551c
Drew DeVault <sir@cmpwn.com>
2014-09-03 19:28:56 -0600

    Remove setTimeout in a silly place

--- js/index.js
+++ js/index.js
@@ -112,27 +112,25 @@ require(["../OpenTI/webui/js/OpenTI/OpenTI"], function(oti) {
 
             openti_log.print("KnightOS "+Pointer_stringify(pointer + 0x64)+" loaded!\n");
 
-            setTimeout(function() {
-                exec("run 10000");
-                exec("unhalt");
+            exec("run 10000");
+            exec("unhalt");
 
-                setTimeout(function tick() {
-                    if (!asic.stopped || asic.cpu.interrupt) {
-                        asic.runloop.tick(asic.clock_rate / 20);
-                    }
-                    if (update_lcd) {
-                        print_lcd(update_lcd);
-                    }
-                    setTimeout(tick, 0);
-                }, 1000 / 60);
+            setTimeout(function tick() {
+                if (!asic.stopped || asic.cpu.interrupt) {
+                    asic.runloop.tick(asic.clock_rate / 20);
+                }
+                if (update_lcd) {
+                    print_lcd(update_lcd);
+                }
+                setTimeout(tick, 0);
+            }, 1000 / 60);
 
-                setTimeout(function tick() {
-                    if (update_lcd) {
-                        print_lcd(update_lcd);
-                    }
-                    setTimeout(tick, 1000 / 60);
-                }, 1000 / 60);
-            }, 500);
+            setTimeout(function tick() {
+                if (update_lcd) {
+                    print_lcd(update_lcd);
+                }
+                setTimeout(tick, 1000 / 60);
+            }, 1000 / 60);
         }
     }
 
