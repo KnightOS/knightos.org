6cf5b078a8dd862cd56774625391c539f10d7f9a
Drew DeVault <sir@cmpwn.com>
2015-03-29 11:08:47 -0600

    Remove userspace version from download page

--- js/download.js
+++ js/download.js
@@ -8,7 +8,6 @@ xhr.onload = function() {
     var build = JSON.parse(this.responseText);
     var name = build.artifacts[0].fileName;
     var version = name.substr(9, name.length - 13);
-    document.getElementById('current-version').textContent = "Version " + version;
     document.getElementById('download-link').href = "http://builds.knightos.org/job/KnightOS/" +
       buildStatus.lastSuccessfulBuild.number + "/artifact/" + name;
     document.getElementById('download-link').removeAttribute('disabled');
